3bae34624998283d574326a23a1be835d07f100d
maysing <kjme23ny@student.ju.se>
2024-02-04 11:35:24 +0100

    Front

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// function setup() {
+//   createCanvas(windowWidth, 500);
+// }
+
+// function draw() {
+//   noStroke();
+//   fill(255, 255, 100);
+//   background(250);
+//   ellipse(200, 200, 500);
+// }
