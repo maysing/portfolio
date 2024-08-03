20310d8d21842e5de1ed06c54b5f2b33f574b5c8
maysing <93945211+maysing@users.noreply.github.com>
2024-08-03 11:18:14 +0200

    design ändring

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 const bouleImg = document.querySelector('.boule-img');
 const bouleText = document.querySelector('#boule-text');
+const osetLogoWhite = document.querySelector('#oset-logo-white');
+const osetDark = document.querySelector('#oset-images');
 
 document.addEventListener("DOMContentLoaded", function() {
   let initialPosition = window.innerWidth - 80;
   let minPosition = 100;
   let scroll500 = 500; // Point at which bouleText reaches full opacity and bouleImg stops moving
   let scroll700 = 700;
-  let scroll1000 = 900; // Point at which bouleImg and bouleText reach zero opacity
+  let scroll900 = 900; // Point at which bouleImg and bouleText reach zero opacity
+  let scroll1300 = 1300;
+  let scroll1400 = 1400; // Point at which osetLogoWhite reaches zero opacity and osetDark reaches full opacity
 
   function handleScroll() {
     let scrollY = window.scrollY;
@@ -26,11 +30,11 @@ document.addEventListener("DOMContentLoaded", function() {
     bouleText.style.opacity = textOpacity;
 
     // Calculate the opacity for bouleImg and bouleText based on scroll position
-    if (scrollY > scroll700 && scrollY <= scroll1000) {
-      let fadeOutOpacity = 1 - ((scrollY - scroll700) / (scroll1000 - scroll700));
+    if (scrollY > scroll700 && scrollY <= scroll900) {
+      let fadeOutOpacity = 1 - ((scrollY - scroll700) / (scroll900 - scroll700));
       bouleImg.style.opacity = fadeOutOpacity;
       bouleText.style.opacity = fadeOutOpacity;
-    } else if (scrollY > scroll1000) {
+    } else if (scrollY > scroll900) {
       bouleImg.style.opacity = "0";
       bouleText.style.opacity = "0";
     } else if (scrollY <= scroll700) {
@@ -38,6 +42,26 @@ document.addEventListener("DOMContentLoaded", function() {
       if (scrollY < scroll500) {
         bouleText.style.opacity = textOpacity;
       }
+    }
+
+    // // Calculate the opacity for osetLogoWhite based on scroll position
+    // if (scrollY > scroll1300 && scrollY <= scroll1400) {
+    //   let logoFadeOutOpacity = 1 - ((scrollY - scroll1300) / (scroll1400 - scroll1300));
+    //   osetLogoWhite.style.opacity = logoFadeOutOpacity;
+    // } else if (scrollY > scroll1400) {
+    //   osetLogoWhite.style.opacity = "0";
+    // } else if (scrollY <= scroll1300) {
+    //   osetLogoWhite.style.opacity = "1";
+    // }
+
+    // Calculate the opacity for osetDark based on scroll position
+    if (scrollY > scroll1300 && scrollY <= scroll1400) {
+      let darkFadeInOpacity = (scrollY - scroll1300) / (scroll1400 - scroll1300);
+      osetDark.style.opacity = darkFadeInOpacity;
+    } else if (scrollY > scroll1400) {
+      osetDark.style.opacity = "1";
+    } else if (scrollY <= scroll1300) {
+      osetDark.style.opacity = "0";
     }
   }
 
